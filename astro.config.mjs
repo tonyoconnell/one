ffdc1ede0c7ac83ce74169482401e5aead160bc8
Anthony OConnell <tony@one.ie>
2023-08-09 14:44:55 +0700

    ONE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [tailwind(), preact()],
 });
