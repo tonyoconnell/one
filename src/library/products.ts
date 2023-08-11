80f95d10362b663dba048b326e65b435a249a539
Anthony OConnell <tony@one.ie>
2023-08-11 18:48:41 +0700

    Add React

---
+++ src/library/products.ts
@@ -0,0 +1,23 @@
+interface Product {
+  id: number
+  title: string
+  description: string
+  price: number
+}
+
+let products: Array<Product>
+
+products = [
+  {
+    id: 0,
+    title: 'Product one',
+    description: 'Product one description',
+    price: 9.99,
+  },
+  {
+    id: 1,
+    title: 'Product two',
+    description: 'Product two description',
+    price: 9.99,
+  },
+]
