c61c5bc82adc4ad3f5f90cc390a1850e5a973c22
Anthony OConnell <tony@one.ie>
2023-08-13 08:31:02 +0700

    Cloudflare Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare(),
   integrations: [tailwind(), svelte(), preact()]
 });
