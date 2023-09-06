30c0a7e6859694d28b09ffbf7f0aea0b1d099af2
Anthony OConnell <tony@one.ie>
2023-09-06 07:32:15 +0700

    Astro 3.08

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
-  integrations: [tailwind(), svelte(), preact()]
+  adapter: vercel(),
+  integrations: [tailwind(), svelte(), preact(), react()]
 });
